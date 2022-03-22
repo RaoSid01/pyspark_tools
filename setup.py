4feff9820986ce1a23b45af08212def3295db0dd
Siddhant Rao <sidrao@bctldsas01.noam.lnrm.net>
2022-03-22 13:38:53 -0400

    fixed syntax err in setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,14 @@ from setuptools import setup
 setup(
     name="pyspark_tools",
     description="Tools for testing pyspark functions",
-    url="https://github.com/RaoSid01/pyspark_tools"
+    url="https://github.com/RaoSid01/pyspark_tools",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     author="Siddhant Rao",
     author_email="siddhant.rao@lexisnexisrisk.com",
     license="Proprietary",
-    packages=[
-        "test_module"
-    ],
-    package_data={
-
-    },
-    scripts=[
-    ],
+    packages=["test_module"],
+    package_data={},
+    scripts=[],
     zip_safe=False,
 )
